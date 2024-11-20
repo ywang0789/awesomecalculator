4ff53bf52574c96fff209614d4a931bf8f6abfee
Wang <115739161+ywang0789@users.noreply.github.com>
2024-11-20 13:33:12 -0500

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dedreira-awesomecalculator", # Replace with your own username
+    name="group7-awesomecalculator",
     version= "1.0.0",
-    author="Daniel Edreira",
-    author_email="dedreira@github.com",
+    author="group7",
+    author_email="ywang0789@github.com",
     description="An awesome calculator package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dedreira/awesomecalculator",
+    url="https://github.com/ywang0789/awesomecalculator",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
